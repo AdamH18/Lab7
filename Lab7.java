7c98f7c9b771cc00f24916a964331edb0333dfd1
AdamH18 <adamh181818@gmail.com>
2016-03-16 15:32:50 -0400

    Finished

--- Lab7.java
+++ Lab7.java
@@ -30,49 +30,67 @@ public class Lab7{
 //    return sentence;
 //  }
   
+  
+  //This does what is described in the prompt where if the word starts with a vowel, it ends in -way.
+  //Some of your examples don't do this.
   public static String findWord(String sentence){
     String word = "";
     int wordSize = sentence.indexOf(" ");
-    for(int i = 0; i < wordSize; i++){
+    if(wordSize == -1)
+      wordSize = sentence.indexOf(".");
+    for(int i = 0; i < wordSize; i++)
       word += sentence.charAt(i);
-    }
-    return word + wordSize;
+    return word;
   }
   
   public static String convertWord(String sentence){
+    boolean vowCheck = true, isVow = true;
+    int ind = 1, end;
     String word = findWord(sentence);
-    int wl = Character.getNumericValue(word.charAt(word.length() - 1));
-    word = word.substring(0, word.length() - 1);
-    if(word.charAt(0) == 101 || word.charAt(0) == 105 || 
-         word.charAt(0) == 111 || word.charAt(0) == 117 || 
-         word.charAt(0) == 125 || word.charAt(0) == 141 || 
-         word.charAt(0) == 145 || word.charAt(0) == 151 || 
-         word.charAt(0) == 157 || word.charAt(0) == 165){
+    if(word.charAt(0) == 65 || word.charAt(0) == 69 || 
+       word.charAt(0) == 73 || word.charAt(0) == 79 || 
+       word.charAt(0) == 85 || word.charAt(0) == 97 || 
+       word.charAt(0) == 101 || word.charAt(0) == 105 || 
+       word.charAt(0) == 111 || word.charAt(0) == 117){
       word += "way";
+      end = 3;
     }
     else{
+      while(vowCheck){
+        if(word.charAt(ind) == 65 || word.charAt(ind) == 69 || 
+           word.charAt(ind) == 73 || word.charAt(ind) == 79 || 
+           word.charAt(ind) == 85 || word.charAt(ind) == 97 || 
+           word.charAt(ind) == 101 || word.charAt(ind) == 105 || 
+           word.charAt(ind) == 111 || word.charAt(ind) == 117){
+          vowCheck = false;
+        }
+        else{
+          ind++;
+          if(ind == word.length() - 1)
+            isVow = false;
+          break;
+        }
+      }
+      if(isVow)
+        word = word.substring(ind) + word.substring(0, ind) + "ay";
+      end = 2;
     }
-    return word;
+    return word + end;
   }
   
   public static String pigLatin(String sentence){
     String newWord, newSentence = ""; 
-    int wl, vowel;
-    while(/*thing*/){
+    int vowel;
+    while(sentence.length() > 0){
       newWord = convertWord(sentence);
+      vowel = Character.getNumericValue(newWord.charAt(newWord.length() - 1));
+      newWord = newWord.substring(0, newWord.length() - 1);
       newSentence += newWord;
-      if(newWord.charAt(0) == 101 || newWord.charAt(0) == 105 || 
-         newWord.charAt(0) == 111 || newWord.charAt(0) == 117 || 
-         newWord.charAt(0) == 125 || newWord.charAt(0) == 141 || 
-         newWord.charAt(0) == 145 || newWord.charAt(0) == 151 || 
-         newWord.charAt(0) == 157 || newWord.charAt(0) == 165){
-        vowel = 3;
-      }
-      else{
-        vowel = 2;
-      }
-      sentence = sentence.substring(newWord.length() - vowel)
+      sentence = sentence.substring(newWord.length() - vowel + 1);
+      if(sentence.length() > 0)
+        newSentence += " ";
     }
+    return newSentence + ".";
   }
 }
 
